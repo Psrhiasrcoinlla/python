f9c1245482a391a71b7ec4d5be7cb1b3cc493b3b
Psrhiasrcoinlla <43026056+Psrhiasrcoinlla@users.noreply.github.com>
2019-07-13 10:37:33 +0530

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,8 @@
+list=[0,20,30,40,50]
+print("print original list:")
+for i in list:
+	print(i,end="")
+ List.remove(0)
+print("\nprinting the list after the removal of first element...")  
+for i in List:  
+    print(i,end=" ")  
